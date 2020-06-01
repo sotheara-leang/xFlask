dd9060df332957e07997eb6faa3a6e3dbca5daf3
sotheara <leangsotheara@gmail.com>
2020-06-01 15:15:14 +0700

    Refactor codes

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     name='xFlask',
     packages=find_packages(),
     package_data={'xflask': ['conf/*']},
-    version='0.1.3',
+    version='0.1.6',
     description='Python Web Framework',
     long_description=readme(),
     long_description_content_type='text/markdown',
